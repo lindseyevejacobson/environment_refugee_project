69617bd7a356e6d426db79c68d37a382a6a8d0fb
Lindsey Jacobson <lindseyevejacobson@gmail.com>
2014-11-30 23:43:50 -0500

    add ability to have multiple markers

---
+++ app/models/location.rb
@@ -0,0 +1,21 @@
+class Location
+
+  def self.all
+    [
+      {"lat"      => 40.591487,
+       "lng"      => -73.804671,
+       "title"    => "Rockaway 113",
+       "desc"     => "In 2012, Hurricane Sandy rocked the shores of New York and New Jersey. Rockaway Beach was one of the places hit the hardest. Rockaway113 shows some of the Environmental Refugees devastated by Superstorm Sandy.",
+       "vimeo_id" => "86156760"},
+
+       {"lat"      => 40.581091,
+         "lng"      => -73.834282,
+         "title"    => "Rockaway 113: Colleen's Story",
+         "desc"     => "In 2012, Hurricane Sandy rocked the shores of New York and New Jersey. Rockaway Beach was one of the places hit the hardest. Rockaway113 shows some of the Environmental Refugees devastated by Superstorm Sandy.",
+         "vimeo_id" => "56177662"}
+
+
+    ]
+  end
+
+end
