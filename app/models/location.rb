82f45c9655d27df7af3209039581190a4de62224
Lindsey Jacobson <lindseyevejacobson@gmail.com>
2014-12-14 18:29:58 -0500

    internucio video

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,6 +14,12 @@ class Location
          "desc"     => "In 2012, Hurricane Sandy rocked the shores of New York and New Jersey. Rockaway Beach was one of the places hit the hardest. Rockaway113 shows some of the Environmental Refugees devastated by Superstorm Sandy.  For more information on Colleen's story <a href='http://cargocollective.com/wwwrockaway113com/2-Colleen' target='blank'> click here</a>",
          "vimeo_id" => "56177662"}
 
+       {"lat"      => 22.212851,
+         "lng"      => 89.816259,
+         "title"    => "Internuncio: Abul's Story",
+         "desc"     => "Approximately 21% of the population, (31.5 million people), live in the coastal regions, these extremely vulnerable people are already noticing the changes in the environment around them. High tides now regularly inundate homes and the land is becoming salty. Fresh water is becoming scarce and the intensity of land erosion is increasing.' target='blank'> click here</a>",
+         "youtube_id" => "0z9uQcw24Os"}
+
 
     ]
   end
