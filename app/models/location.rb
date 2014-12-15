fff4a7e73fe824e7ca5c9bc125f01830a919268d
Andrew Glass <andrewglass1@gmail.com>
2014-12-14 23:34:31 -0500

    add a comma in the list of locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -12,7 +12,7 @@ class Location
          "lng"      => -73.834282,
          "title"    => "Rockaway 113: Colleen's Story",
          "desc"     => "In 2012, Hurricane Sandy rocked the shores of New York and New Jersey. Rockaway Beach was one of the places hit the hardest. Rockaway113 shows some of the Environmental Refugees devastated by Superstorm Sandy.  For more information on Colleen's story <a href='http://cargocollective.com/wwwrockaway113com/2-Colleen' target='blank'> click here</a>",
-         "vimeo_id" => "56177662"}
+         "vimeo_id" => "56177662"},
 
        {"lat"      => 22.212851,
          "lng"      => 89.816259,
